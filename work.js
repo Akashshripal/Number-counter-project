8c995fdb91a560f995593089f9231f85664f8865
Akashshripal <98543501+Akashshripal@users.noreply.github.com>
2022-05-19 19:28:11 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,33 @@
+//set fixed value
+let count= 0;
+
+//slect value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+console.log(btns);
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('Decrease')){
+         count--;
+         } else if(styles.contains('Increase')){
+       count++;
+        }
+        else{
+            count=0;
+        }
+        if(count>0){
+            value.style.color="green";
+
+        }
+        if(count<0){
+            value.style.color="red";
+        }
+        if(count==0){
+            value.style.color="#222";
+        }
+        value.textContent = count;
+    });
+});
